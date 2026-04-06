d4327406bc7aaec671b2c69b6d6aa5b8f4f35a23
Magnamelamon <rodriguez.silva.christopher@gmail.com>
2026-04-06 16:15:54 -0600

    fix: scroll to top on PDP load

--- src/pages/ProductoDetalle/ProductoDetalle.tsx
+++ src/pages/ProductoDetalle/ProductoDetalle.tsx
@@ -14,6 +14,7 @@ const ProductoDetalle = () => {
   const [productosRelacionados, setProductosRelacionados] = useState<Producto[]>([]);
 
   useEffect(() => {
+    window.scrollTo(0, 0);
     const fetchProduct = async () => {
       setCargando(true);
       try {
